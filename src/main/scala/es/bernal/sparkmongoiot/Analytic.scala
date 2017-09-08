8fbb9d06142f491be6076277522afa381a1612ce
Jorge Bernal Chaves <restless_project@hotmail.es>
2017-09-09 01:19:04 +0200

    Analyzer type class incuded and refactor

--- src/main/scala/es/bernal/sparkmongoiot/Analytic.scala
+++ src/main/scala/es/bernal/sparkmongoiot/Analytic.scala
@@ -41,25 +41,34 @@ object Analytic {
                         val hdfsHostAndPort: String = Constants.hdfsHostPort
                         ) extends DataLoader
 
+  trait AnalyticsMode // W classes
+  class StaticAnalytics() extends AnalyticsMode
+  class DynamicsAnalytics() extends AnalyticsMode
+
 
   // Type Class interfaces
-  trait Configurator[T] {
+  trait Configurator[T,W] {
     def getSparkSessionBuilder(t: T): SparkSession.Builder
     def getHadoopRdd(t: T, sparkSession: SparkSession): RDD[String]
   }
 
-  trait OriginDataHandler[T,V] {
-    def getBaseDataFrame(c: Configurator[T], t: T, v: V, sparkSession: SparkSession): DataFrame
+  trait OriginDataHandler[T,V,W] {
+    def getBaseDataFrame(c: Configurator[T,W], t: T, v: V, sparkSession: SparkSession): DataFrame
+  }
+
+  trait Analyzer[W] {
+    def handleDataFrame(sparkSession: SparkSession, df: DataFrame): Unit
   }
 
   object Configurator extends ConfiguratorInstances
   object OriginDataHandler extends OriginDataHandlerInstances
+  object Analayzer extends AnalyzerInstances
 
-  // Type Class instances
+  // T Type Class instances
   trait ConfiguratorInstances {
-    def apply[T](implicit ev: Configurator[T]) = ev
+    def apply[T,W](implicit ev: Configurator[T,W]) = ev
 
-    implicit def homeInstance = new Configurator[Home] {
+    implicit def homeInstance = new Configurator[Home, StaticAnalytics] {
       def getSparkSessionBuilder(t: Home): SparkSession.Builder = {
         val ssBuilder = SparkSession.builder()
           .appName("spark-mongo-iot")
@@ -73,7 +82,7 @@ object Analytic {
       }
     }
 
-    implicit def localInstance = new Configurator[Local] {
+    implicit def localInstance = new Configurator[Local, StaticAnalytics] {
       def getSparkSessionBuilder(t: Local): SparkSession.Builder = {
         val ssBuilder = SparkSession.builder()
           .appName("spark-mongo-iot")
@@ -87,7 +96,7 @@ object Analytic {
       }
     }
 
-    implicit def remoteClusterInstance = new Configurator[RemoteCluster] {
+    implicit def remoteClusterInstance = new Configurator[RemoteCluster, StaticAnalytics] {
       def getSparkSessionBuilder(t: RemoteCluster): SparkSession.Builder = {
         val ssBuilder = SparkSession.builder()
           .appName("spark-mongo-iot")
@@ -102,44 +111,44 @@ object Analytic {
 
   }
 
-  // Type Class instances
+  // V Type Class instances
   trait OriginDataHandlerInstances {
-    def apply[T,V](implicit ev: OriginDataHandler[T,V]) = ev
+    def apply[T,V,W](implicit ev: OriginDataHandler[T,V,W]) = ev
 
     // Hadoop solution
-    implicit def homeByHadoopInstance = new OriginDataHandler[Home, HadoopLoader] {
-      def getBaseDataFrame(c: Configurator[Home], t: Home, v: HadoopLoader, sparkSession: SparkSession): DataFrame = {
+    implicit def homeByHadoopInstance = new OriginDataHandler[Home, HadoopLoader, StaticAnalytics] {
+      def getBaseDataFrame(c: Configurator[Home, StaticAnalytics], t: Home, v: HadoopLoader, sparkSession: SparkSession): DataFrame = {
         getHadoopDataFrame(c, t, sparkSession)
       }
     }
-    implicit def localByHadoopInstance = new OriginDataHandler[Local, HadoopLoader] {
-      def getBaseDataFrame(c: Configurator[Local], t: Local, v: HadoopLoader, sparkSession: SparkSession): DataFrame = {
+    implicit def localByHadoopInstance = new OriginDataHandler[Local, HadoopLoader, StaticAnalytics] {
+      def getBaseDataFrame(c: Configurator[Local, StaticAnalytics], t: Local, v: HadoopLoader, sparkSession: SparkSession): DataFrame = {
         getHadoopDataFrame(c, t, sparkSession)
       }
     }
-    implicit def remoteClusterByHadoopInstance = new OriginDataHandler[RemoteCluster, HadoopLoader] {
-      def getBaseDataFrame(c: Configurator[RemoteCluster], t: RemoteCluster, v: HadoopLoader, sparkSession: SparkSession): DataFrame = {
+    implicit def remoteClusterByHadoopInstance = new OriginDataHandler[RemoteCluster, HadoopLoader, StaticAnalytics] {
+      def getBaseDataFrame(c: Configurator[RemoteCluster, StaticAnalytics], t: RemoteCluster, v: HadoopLoader, sparkSession: SparkSession): DataFrame = {
         getHadoopDataFrame(c, t, sparkSession)
       }
     }
-    def getHadoopDataFrame[T](c: Configurator[T], t: T, sparkSession: SparkSession): DataFrame = {
+    def getHadoopDataFrame[T,W](c: Configurator[T,W], t: T, sparkSession: SparkSession): DataFrame = {
       val hadoopRdd: RDD[String] = c.getHadoopRdd(t, sparkSession)
       sparkSession.read.json(hadoopRdd)
     }
 
     // MongoDb Solution
-    implicit def homeBymongoDbInstance = new OriginDataHandler[Home, MongoDbLoader] {
-      def getBaseDataFrame(c: Configurator[Home], t: Home, v: MongoDbLoader, sparkSession: SparkSession): DataFrame = {
+    implicit def homeBymongoDbInstance = new OriginDataHandler[Home, MongoDbLoader,StaticAnalytics] {
+      def getBaseDataFrame(c: Configurator[Home,StaticAnalytics], t: Home, v: MongoDbLoader, sparkSession: SparkSession): DataFrame = {
         getMongoDbBaseDataFrame(v, sparkSession)
       }
     }
-    implicit def localBymongoDbInstance = new OriginDataHandler[Local, MongoDbLoader] {
-      def getBaseDataFrame(c: Configurator[Local], t: Local, v: MongoDbLoader, sparkSession: SparkSession): DataFrame = {
+    implicit def localBymongoDbInstance = new OriginDataHandler[Local, MongoDbLoader, StaticAnalytics] {
+      def getBaseDataFrame(c: Configurator[Local, StaticAnalytics], t: Local, v: MongoDbLoader, sparkSession: SparkSession): DataFrame = {
         getMongoDbBaseDataFrame(v, sparkSession)
       }
     }
-    implicit def remoteClusterBymongoDbInstance = new OriginDataHandler[RemoteCluster, MongoDbLoader] {
-      def getBaseDataFrame(c: Configurator[RemoteCluster], t: RemoteCluster, v: MongoDbLoader, sparkSession: SparkSession): DataFrame = {
+    implicit def remoteClusterBymongoDbInstance = new OriginDataHandler[RemoteCluster, MongoDbLoader, StaticAnalytics] {
+      def getBaseDataFrame(c: Configurator[RemoteCluster, StaticAnalytics], t: RemoteCluster, v: MongoDbLoader, sparkSession: SparkSession): DataFrame = {
         getMongoDbBaseDataFrame(v, sparkSession)
       }
     }
@@ -156,83 +165,99 @@ object Analytic {
 
   }
 
+  // W Type Class instances
+  trait AnalyzerInstances {
+    def apply[W](implicit ev: Analyzer[W]) = ev
+
+    implicit def staticAnalyzerInstance = new Analyzer[StaticAnalytics] {
+      def handleDataFrame(sparkSession: SparkSession, df: DataFrame): Unit = {
+        import sparkSession.sqlContext.implicits._
+        val rddProjected = df.select(col("organizationId"),col("channelId"),
+          col("datastreamId"), col("deviceId"), col("date.epoch").as("date_epoch"),
+          col("value")).cache
+
+        // Extract list of datastreams
+        val datastreams = rddProjected.select("datastreamId").distinct()
+
+        val it = datastreams.toLocalIterator()
+        val dsList = new ListBuffer[String]()
+        while (it.hasNext) {
+          val ds = it.next().getAs[String]("datastreamId")
+          dsList += ds
+        }
+
+        val allDs = dsList.toList.par
+
+        allDs.foreach(ds => {
+          val rddForDs = rddProjected.where($"datastreamId" === ds)
+
+          if (isValueContinuous(ds)) {
+            val rddForDsAgg = rddForDs
+              .groupBy("deviceId", "organizationId", "channelId", "datastreamId")
+              .agg(count("value").as("count"),avg("value").as("avg"),stddev("value").as("stddev")
+                ,max("value").as("max"), min("value").as("min")
+              )
+
+            // write countinous analytic
+            val rddDocs: RDD[Document] = rddForDsAgg.rdd.map(r => {
+              val dpa = new DataPointAnalyticCnt(r.getAs[String]("deviceId"), r.getAs[String]("organizationId"), r.getAs[String]("channelId"),
+                r.getAs[String]("datastreamId"),
+                Stats(r.getAs[Long]("count"), r.getAs[Double]("avg"), r.getAs[Double]("stddev")
+                  , r.getAs[String]("max").toDouble, r.getAs[String]("min").toDouble
+                ))
+              implicit val formats = DefaultFormats
+              val jsonStr = write(dpa)
+              Document.parse(jsonStr)
+            })
+            MongoSpark.save(rddDocs)
+          } else {
+            val rddForDsAgg = rddForDs
+              .groupBy("deviceId", "organizationId", "channelId", "datastreamId", "value")
+              .count()
+
+            // write discrete analytic
+            val rddForDsAggMap = rddForDsAgg.rdd.map(r => ((r.getAs[String]("deviceId"), r.getAs[String]("organizationId"), r.getAs[String]("channelId"), r.getAs[String]("datastreamId")),r))
+              .groupByKey()
+
+            val rddDocs: RDD[Document] = rddForDsAggMap.map(t => {
+              var accs = ListBuffer[Accumulator]()
+              t._2.foreach(r => {
+                accs += Accumulator(r.getAs[String]("value"), r.getAs[Long]("count"))
+              })
+              val dpa = DataPointAnalyticDct(t._1._1, t._1._2, t._1._3, t._1._4, accs.toList)
+              implicit val formats = DefaultFormats
+              val jsonStr = write(dpa)
+              Document.parse(jsonStr)
+            })
+            MongoSpark.save(rddDocs)
+          }
+
+        })
+      }
+    }
+    implicit def dynamicAnalyzerInstance = new Analyzer[DynamicsAnalytics] {
+      def handleDataFrame(sparkSession: SparkSession, df: DataFrame): Unit = {
+        // TODO start Thrift Server ...
+      }
+    }
+  }
+
+
   // Polymorphical function
-  def analyticFunction[T,V](configurator: Configurator[T], originDataHandler: OriginDataHandler[T,V], t: T, v: V): Unit = {
+  def analyticFunction[T,V,W](configurator: Configurator[T,W], originDataHandler: OriginDataHandler[T,V,W], analyzer: Analyzer[W], t: T, v: V): Unit = {
 
     println("=> starting spark-mongo-iot sample")
 
     val ssBuilder = configurator.getSparkSessionBuilder(t)
     val ss = ssBuilder.getOrCreate()
-    import ss.sqlContext.implicits._
 
     println("=> config loaded!")
     val timeini: Long = System.currentTimeMillis()
 
-    var rdd: DataFrame = originDataHandler.getBaseDataFrame(configurator, t, v, ss);
+    var rdd: DataFrame = originDataHandler.getBaseDataFrame(configurator, t, v, ss)
 
     // From here, work with DataFrame
-
-    val rddProjected = rdd.select(col("organizationId"),col("channelId"),
-      col("datastreamId"), col("deviceId"), col("date.epoch").as("date_epoch"),
-      col("value")).cache
-
-    // Extract list of datastreams
-    val datastreams = rddProjected.select("datastreamId").distinct()
-
-    val it = datastreams.toLocalIterator()
-    val dsList = new ListBuffer[String]()
-    while (it.hasNext) {
-      val ds = it.next().getAs[String]("datastreamId")
-      dsList += ds
-    }
-
-    val allDs = dsList.toList.par
-
-    allDs.foreach(ds => {
-      val rddForDs = rddProjected.where($"datastreamId" === ds)
-
-      if (isValueContinuous(ds)) {
-        val rddForDsAgg = rddForDs
-          .groupBy("deviceId", "organizationId", "channelId", "datastreamId")
-          .agg(count("value").as("count"),avg("value").as("avg"),stddev("value").as("stddev")
-            ,max("value").as("max"), min("value").as("min")
-          )
-
-        // write countinous analytic
-        val rddDocs: RDD[Document] = rddForDsAgg.rdd.map(r => {
-          val dpa = new DataPointAnalyticCnt(r.getAs[String]("deviceId"), r.getAs[String]("organizationId"), r.getAs[String]("channelId"),
-            r.getAs[String]("datastreamId"),
-            Stats(r.getAs[Long]("count"), r.getAs[Double]("avg"), r.getAs[Double]("stddev")
-              , r.getAs[String]("max").toDouble, r.getAs[String]("min").toDouble
-            ))
-          implicit val formats = DefaultFormats
-          val jsonStr = write(dpa)
-          Document.parse(jsonStr)
-        })
-        MongoSpark.save(rddDocs)
-      } else {
-        val rddForDsAgg = rddForDs
-          .groupBy("deviceId", "organizationId", "channelId", "datastreamId", "value")
-          .count()
-
-        // write discrete analytic
-        val rddForDsAggMap = rddForDsAgg.rdd.map(r => ((r.getAs[String]("deviceId"), r.getAs[String]("organizationId"), r.getAs[String]("channelId"), r.getAs[String]("datastreamId")),r))
-          .groupByKey()
-
-        val rddDocs: RDD[Document] = rddForDsAggMap.map(t => {
-          var accs = ListBuffer[Accumulator]()
-          t._2.foreach(r => {
-            accs += Accumulator(r.getAs[String]("value"), r.getAs[Long]("count"))
-          })
-          val dpa = DataPointAnalyticDct(t._1._1, t._1._2, t._1._3, t._1._4, accs.toList)
-          implicit val formats = DefaultFormats
-          val jsonStr = write(dpa)
-          Document.parse(jsonStr)
-        })
-        MongoSpark.save(rddDocs)
-      }
-
-    })
+    analyzer.handleDataFrame(ss, rdd)
 
     val timeend: Long = System.currentTimeMillis()
 
@@ -252,14 +277,14 @@ object Analytic {
   def main(args: Array[String]): Unit = {
     if (args.length == 0) {
       // home or local case, depending on what to test (mongo load by default)
-      analyticFunction(Configurator.homeInstance, OriginDataHandler.homeBymongoDbInstance, new Home(), new MongoDbLoader())
+      analyticFunction(Configurator.homeInstance, OriginDataHandler.homeBymongoDbInstance, Analayzer.staticAnalyzerInstance, new Home(), new MongoDbLoader())
     } else if (args.length == 7) {
       if (args(6).equals(Constants.modeMongoConn)) {
         // RemoteCluster Mongo originated data
-        analyticFunction(Configurator.remoteClusterInstance, OriginDataHandler.remoteClusterBymongoDbInstance, new RemoteCluster(args), new MongoDbLoader())
+        analyticFunction(Configurator.remoteClusterInstance, OriginDataHandler.remoteClusterBymongoDbInstance, Analayzer.staticAnalyzerInstance, new RemoteCluster(args), new MongoDbLoader())
       } else {
         // RemoteCluster Hadoop originated data
-        analyticFunction(Configurator.remoteClusterInstance, OriginDataHandler.remoteClusterByHadoopInstance, new RemoteCluster(args), new HadoopLoader(args(6)))
+        analyticFunction(Configurator.remoteClusterInstance, OriginDataHandler.remoteClusterByHadoopInstance, Analayzer.staticAnalyzerInstance, new RemoteCluster(args), new HadoopLoader(args(6)))
       }
     }
   }
